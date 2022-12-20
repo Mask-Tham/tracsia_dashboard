8c53f63c6e5fce82822bb6bcff2c0e8fe27885cf
amy29480 <amyexom@gmail.com>
2022-12-20 23:25:08 +0700

    update sidebar

--- src/views/dashboards/smart-farm/dashboard/farm-monitoring-dashboard.vue
+++ src/views/dashboards/smart-farm/dashboard/farm-monitoring-dashboard.vue
@@ -334,4 +334,23 @@ export default {
   background: #2EC094;
   width: 100px;
 }
+/* width */
+::-webkit-scrollbar {
+  width: 7px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
 </style>
