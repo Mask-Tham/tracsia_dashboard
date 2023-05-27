ac8ed3cf2997c93b3e4bc7dd92d868c78593704c
amy29480 <amyexom@gmail.com>
2023-05-27 14:33:27 +0700

    update

--- src/views/report/search.vue
+++ src/views/report/search.vue
@@ -1,224 +1,137 @@
 <template>
-    <v-card flat class="">
-      <v-card-text class="pt-0">
-        <v-progress-linear :active="loading" :indeterminate="loading" absolute></v-progress-linear>
-      </v-card-text>
-      <v-form v-model="valid" ref="formChangePass" lazy-validation>
-        <div class="px-3">
-          <v-card-text class="pt-5">
-            <v-row>
-              <v-col cols="2" >
-                <!-- current password -->
-                <v-text-field
-                  v-model="currentPassword"
-                  :type="isCurrentPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isCurrentPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="Current Password"
-                  :rules="[validators.required]"
-                  outlined
-                  dense
-                  @click:append="isCurrentPasswordVisible = !isCurrentPasswordVisible"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="2" >
-                <!-- new password -->
-                <v-text-field
-                  v-model="newPassword"
-                  :type="isNewPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isNewPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="New Password"
-                  :rules="[
-                    validators.required,
-                    validators.passwordValidator2,
-                    newPassword != currentPassword || 'New Password equal Current Password',
-                  ]"
-                  outlined
-                  dense
-                  persistent-hint
-                  @click:append="isNewPasswordVisible = !isNewPasswordVisible"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="2" >
-                <!-- confirm password -->
-                <v-text-field
-                  v-model="cPassword"
-                  :type="isCPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isCPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="Confirm New Password"
-                  :rules="[validators.required, newPassword === cPassword || 'Password not equal New Password']"
-                  outlined
-                  dense
-                  @click:append="isCPasswordVisible = !isCPasswordVisible"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="2" >
-                <!-- current password -->
-                <v-text-field
-                  v-model="currentPassword"
-                  :type="isCurrentPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isCurrentPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="Current Password"
-                  :rules="[validators.required]"
-                  outlined
-                  dense
-                  @click:append="isCurrentPasswordVisible = !isCurrentPasswordVisible"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="2" >
-                <!-- new password -->
-                <v-text-field
-                  v-model="newPassword"
-                  :type="isNewPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isNewPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="New Password"
-                  :rules="[
-                    validators.required,
-                    validators.passwordValidator2,
-                    newPassword != currentPassword || 'New Password equal Current Password',
-                  ]"
-                  outlined
-                  dense
-                  persistent-hint
-                  @click:append="isNewPasswordVisible = !isNewPasswordVisible"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="2" >
-                <!-- confirm password -->
-                <v-text-field
-                  v-model="cPassword"
-                  :type="isCPasswordVisible ? 'text' : 'password'"
-                  :append-icon="isCPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
-                  label="Confirm New Password"
-                  :rules="[validators.required, newPassword === cPassword || 'Password not equal New Password']"
-                  outlined
-                  dense
-                  @click:append="isCPasswordVisible = !isCPasswordVisible"
-                ></v-text-field>
-              </v-col>
-  
-              <!-- <v-col
-                cols="12"
-                sm="4"
-                md="6"
-                class="d-none d-sm-flex justify-center position-relative"
-              >
-                <v-img
-                  contain
-                  max-width="170"
-                  src="@/assets/images/3d-characters/pose-m-1.png"
-                  class="security-character"
-                ></v-img>
-              </v-col> -->
-            </v-row>
-          </v-card-text>
-        </div>
-  
-        <!-- divider -->
-        <v-divider></v-divider>
-  
-        <div class="pa-3">
-          <!-- <v-card-title class="flex-nowrap">
-            <v-icon class="text--primary me-3">
-              {{ icons.mdiKeyOutline }}
-            </v-icon>
-            <span class="text-break">Two-factor authentication</span>
-          </v-card-title> -->
-  
-          <!-- <v-card-text class="two-factor-auth text-center mx-auto">
-            <v-avatar
-              color="primary"
-              class="v-avatar-light-bg primary--text mb-4"
-              rounded
-            >
-              <v-icon
-                size="25"
-                color="primary"
-              >
-                {{ icons.mdiLockOpenOutline }}
-              </v-icon>
-            </v-avatar>
-            <p class="text-base text--primary font-weight-semibold">
-              Two factor authentication is not enabled yet.
-            </p>
-            <p class="text-sm text--primary">
-              Two-factor authentication adds an additional layer of
-              security to your account by requiring more than just a
-              password to log in. Learn more.
-            </p>
-          </v-card-text> -->
-  
-          <!-- success -->
-          <v-card-text>
-            <alert :isShow="alertSuc" :message="success" type="success"></alert>
-          </v-card-text>
-  
-          <!-- alert -->
-          <v-card-text>
-            <alert :isShow="alert" :message="error"></alert>
-          </v-card-text>
-  
-          <!-- action buttons -->
-          <v-card-text>
-            <v-btn color="primary" class="me-3 mt-3" @click="ChangePassword"> Save changes </v-btn>
-            <v-btn color="secondary" outlined class="mt-3"> Cancel </v-btn>
-          </v-card-text>
-        </div>
-      </v-form>
-    </v-card>
-  </template>
-  
-  <script>
-  // eslint-disable-next-line object-curly-newline
-  import { passwordValidator2, required } from '@core/utils/validation'
-import { mdiEyeOffOutline, mdiEyeOutline, mdiKeyOutline, mdiLockOpenOutline } from '@mdi/js'
-  // import { ref } from '@vue/composition-api'
-  import Alert from '@/utils/Alert.vue'
-  
-  export default {
-    setup() {
-      return {
-        icons: {
-          mdiKeyOutline,
-          mdiLockOpenOutline,
-          mdiEyeOffOutline,
-          mdiEyeOutline,
-        },
-        validators: {
-          required,
-          passwordValidator2,
-        },
-      }
-    },
-    components: { Alert },
-    data() {
-      return {
-        isCurrentPasswordVisible: false,
-        isNewPasswordVisible: false,
-        isCPasswordVisible: false,
-        currentPassword: '',
-        newPassword: '',
-        cPassword: '',
-        valid: false,
-        error: '',
-        alert: false,
-        success: '',
-        alertSuc: false,
-        loading: false,
-      }
-    },
-    methods: {
+  <div id="user-list">
+    <!-- list filters -->
+    <v-card>
      
-    },
-  }
-  </script>
-  
-  <style lang="scss" scoped>
-  .two-factor-auth {
-    max-width: 25rem;
-  }
-  .security-character {
-    position: absolute;
-    bottom: -0.5rem;
-  }
-  </style>
-  
+      <!-- actions -->
+      <v-card-text class="d-flex align-center flex-wrap pb-0">
+        <!-- search -->
+        <v-text-field
+          v-model="searchTable"
+          placeholder="Search"
+          outlined
+          hide-details
+          dense
+          class="user-search me-3 mb-4"
+        >
+        </v-text-field>
+
+        <v-select
+          v-model="selectedMonth"
+          placeholder="Select Month"
+          :items="month"
+          item-text="title"
+          item-value="value"
+          outlined
+          dense
+          clearable
+          class="user-search me-3 mb-4"
+          hide-details
+        ></v-select>
+
+        <v-select
+          v-model="selectedYear"
+          placeholder="Select Year"
+          :items="year"
+          item-text="custumerID"
+          item-value="custumerID"
+          outlined
+          dense
+          clearable
+          class="user-search me-3 mb-4"
+          hide-details
+        ></v-select>
+
+        <v-spacer></v-spacer>
+
+        <div class="d-flex align-center flex-wrap">
+          <!-- <v-btn color="primary" class="mb-4 me-3" @click.stop="openCreate">
+            <v-icon>{{ icons.mdiPlus }}</v-icon>
+            <span>Add New Role</span>
+          </v-btn> -->
+
+          <v-btn color="secondary" outlined class="mb-4">
+            <v-icon size="17" class="me-1">
+              {{ icons.mdiExportVariant }}
+            </v-icon>
+            <span>Export</span>
+          </v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line object-curly-newline
+import {
+  mdiAccountOutline,
+  mdiDeleteOutline,
+  mdiDotsVertical,
+  mdiExportVariant,
+  mdiFileDocumentOutline,
+  mdiPencil,
+  mdiPlus,
+  mdiSquareEditOutline,
+} from '@mdi/js'
+
+// sidebar
+
+export default {
+  components: {},
+  setup() {
+    return {
+      // icons
+      icons: {
+        mdiSquareEditOutline,
+        mdiFileDocumentOutline,
+        mdiDotsVertical,
+        mdiDeleteOutline,
+        mdiPlus,
+        mdiExportVariant,
+        mdiAccountOutline,
+        mdiPencil,
+      },
+    }
+  },
+  data() {
+    return {
+      isAddNewUserSidebarActive: false,
+      userTotalLocal: [],
+      selectedMonth: null,
+      month: [
+        // { title: 'on', value: 'true' },
+        // { title: 'off', value: 'false' },
+      ],
+      selectedYear: '',
+      hearderTable: [
+        { text: 'Customer ID', value: 'custumerID' },
+        { text: 'Role ID', value: 'roleID' },
+        { text: 'Description', value: 'description' },
+        {
+          text: 'STATUS',
+          value: 'isUse',
+          align: 'center',
+        },
+        { text: 'Date Create', value: 'date' },
+        {
+          text: 'ACTION',
+          value: 'actions',
+          align: 'center',
+          sortable: false,
+        },
+      ],
+      year: [],
+      selectedRows: [],
+      loadingTable: false,
+      searchTable: '',
+      propRoleData: undefined,
+    }
+  },
+  computed: {},
+  mounted() {},
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/report/sensor.vue
@@ -0,0 +1,135 @@
+<template>
+<div style="padding: 10px;">
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template v-slot:item.name="{ item }">
+      <v-icon :color="'#84E0BE'" size="20">{{ icons.mdiSignal}}</v-icon> Lora Sensor {{ item.id }}
+    </template>
+    <template v-slot:item.carbs="{ item }">
+      <v-icon :color="'#84E0BE'" size="20">{{ icons.mdiBattery}}</v-icon>
+    </template>
+  </v-data-table>
+</div>
+</template>
+
+<script>
+import { mdiBattery, mdiCheckCircle, mdiCloseCircle, mdiSignal } from '@mdi/js'
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Sensor Name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Sensor Type', value: 'type' },
+        { text: 'Reading Data', value: 'fat' },
+        { text: 'Battery', value: 'carbs' },
+        { text: 'Timestamp', value: 'protein' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          type: 'Lora',
+          fat: 'rssi: -55',
+          carbs: 24,
+          protein: '2022-02-23 16:34:20',
+          id: 1,
+        },
+        {
+          name: 'Ice cream sandwich',
+          type: 'Lora',
+          fat: 'humiditySensor: 9.0%',
+          carbs: 37,
+          protein: '2022-02-27 18:34:41',
+          id: 2,
+        },
+        {
+          name: 'Eclair',
+          type: 'Lora',
+          protein: '2022-02-27 18:34:41',
+          carbs: 23,
+          fat: 'humiditySensor: 7.0%',
+          id: 3,
+        },
+        {
+          name: 'Cupcake',
+          type: 'Lora',
+          fat: 'rssi: 3.7',
+          carbs: 67,
+          protein: '2022-02-28 11:12:08',
+          id: 4,
+        },
+        {
+          name: 'Gingerbread',
+          type: 'Lora',
+          fat: 'temperatureSensor: 31.0°C',
+          carbs: 49,
+          protein: '2022-02-28 18:34:39',
+          id: 16,
+        },
+        {
+          name: 'Jelly bean',
+          type: 'Lora',
+          fat: 'temperatureSensor: 36.0°C',
+          carbs: 94,
+          protein: '2022-02-28 18:34:39',
+          id: 70,
+        },
+        {
+          name: 'Lollipop',
+          type: 'Lora',
+          fat: 'humiditySensor: 9.0%',
+          carbs: 98,
+          protein: '2022-02-28 18:34:39',
+          id: 2,
+        },
+        {
+          name: 'Honeycomb',
+          type: 'Lora',
+          fat: 'temperatureSensor: 36.0°C',
+          carbs: 87,
+          protein: '2022-02-28 18:34:39',
+          id: 45,
+        },
+        {
+          name: 'Donut',
+          type: 'Lora',
+          fat: 'temperatureSensor: 36.0°C',
+          carbs: 51,
+          protein: '2022-03-30 18:34:39',
+          id: 22,
+        },
+        {
+          name: 'KitKat',
+          type: 'Lora',
+          fat: 'rssi: -35',
+          carbs: 65,
+          protein: '2022-03-30 18:34:39',
+          id: 6,
+        },
+      ],
+      icons: {
+        mdiSignal,
+        mdiBattery,
+        mdiCheckCircle,
+        mdiCloseCircle,
+      },
+    }
+  },
+  methods: {
+    getColor(calories) {
+      if (calories > 400) return 'red'
+      else if (calories > 200) return 'orange'
+      else return 'green'
+    },
+  },
+}
+</script>
+<style  scoped>
+</style>
